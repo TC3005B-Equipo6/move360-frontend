e29e2eece5bc6ff935c43f0fd0bb968c1787cb06
saraxfl <A07045944@tec.mx>
2026-04-14 20:48:23 -0600

    Componentes
    Se agregan los tres componentes y se implementa storybook

---
+++ src/stories/ButtonCreate.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { CreateButton } from "../components/Button/ButtonCreate";
+
+const meta: Meta<typeof CreateButton> = {
+  title: "Components/CreateButton",
+  component: CreateButton,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof CreateButton>;
+
+export const Dashboard: Story = {
+  args: {
+    variant: "primary",
+    size: "medium",
+    label: "Nuevo dashboard +",
+    onPress: () => console.log("New dashboard"),
+  },
+};
+
+export const Grafico: Story = {
+  args: {
+    variant: "secondary",
+    size: "small",
+    label: "Nueva gráfica +",
+    onPress: () => console.log("New graph"),
+  },
+};
+
+export const Eliminar: Story = {
+  args: {
+    variant: "danger",
+    size: "small",
+    label: "Eliminar",
+    onPress: () => console.log("Eliminar"),
+  },
+};
