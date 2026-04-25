858e039a7455f5656f8b865f4b3df094e6054553
saraxfl <A07045944@tec.mx>
2026-04-24 18:40:53 -0600

    Conexion con firebase (#30)

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBJLmzYNRnJbhvGhIvNwlFm-YmSjtARSUA",
+
+  authDomain: "move360-c02b3.firebaseapp.com",
+
+  projectId: "move360-c02b3",
+
+  storageBucket: "move360-c02b3.firebasestorage.app",
+
+  messagingSenderId: "410306255400",
+
+  appId: "1:410306255400:web:019ebe4e6a0db186dbb31d"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
