3956476af5e580160c7fd1821204d89b75a93166
saraxfl <A07045944@tec.mx>
2026-04-25 15:04:51 -0600

    MV-25Autenticacion
    * MV-25 Endpoint authentication
    
    * MV-25Autenticación

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -11,3 +11,22 @@ const user = userCredential.user;
 const token = await user.getIdToken();
 return token;
 };
+
+
+export async function validateToken() {
+  const token = localStorage.getItem("token");
+
+  console.log("Token enviado:", token);
+
+  const response = await fetch("http://localhost:8080/auth/validate", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const data = await response.text();
+  console.log("Respuesta backend:", data);
+
+  return data;
+}
