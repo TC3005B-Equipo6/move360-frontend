7e3f6a749a9e14191d3a907ef80283ad9051c0e3
doyDynamo <dho2000@hotmail.com>
2026-05-14 14:39:58 -0600

    feat: refactor header, dashboard

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,20 @@ import { IconButton } from "../IconButton/IconButton";
 
 export type HeaderProps = {
   title: string;
+  showReport?: boolean;
+  showSettings?: boolean;
 };
 
-export function Header({ title = "Hola" }: HeaderProps) {
+export function Header({ title = "Hola", showReport = false, showSettings = false }: HeaderProps) {
   return (
     <div className="bg-white rounded-[20px] flex items-center justify-between h-[100px] w-full px-5 font-[Inter,sans-serif] text-[#1F4E79] text-[35px] font-light gap-5">
       <>{title}</>
-      <div className="flex gap-[25px]">
-        <IconButton size="large" iconName="report" label="Reporte" color="secondary" />
-        <IconButton size="small" iconName="settings" label="" color="secondary" />
-      </div>
+      {(showReport || showSettings) && (
+        <div className="flex gap-[25px]">
+          {showReport && <IconButton size="large" iconName="report" label="Reporte" color="secondary" />}
+          {showSettings && <IconButton size="small" iconName="settings" label="" color="secondary" />}
+        </div>
+      )}
     </div>
   );
 }
