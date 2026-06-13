1bf2a96d679e355dc16eec689cd0bed75a7939ee
aneleanor <A01665593@tec.mx>
2026-06-12 20:16:38 -0600

    Feature/protected routes (#66)
    * feature/missing stories
    
    * feature/add protection to the routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,22 @@ import DashboardDetail from './screens/Dashboard/DashboardDetail';
 import Explore from './screens/Explore/Explore';
 import TestDashboard from './screens/TestDashboard/TestDashboard';
 import Showcase from './screens/Showcase/Showcase';
+import { ProtectedRoute } from './components/navigation/ProtectedRoute/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/:dashboardId" element={<DashboardDetail />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/test" element={<TestDashboard />} />
-        <Route path="/showcase" element={<Showcase />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/:dashboardId" element={<DashboardDetail />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/test" element={<TestDashboard />} />
+          <Route path="/showcase" element={<Showcase />} />
+        </Route>
       </Routes>
     </Router>
   );
